ae0e4510dfb2609d20ff751c3fed98d512c4ad11
Arun Wilson <007arunwilson@gmail.com>
2021-10-25 16:13:22 +0530

    new styles added

---
+++ assets/js/script.js
@@ -0,0 +1,97 @@
+// let cookingtable = document.getElementById("CookingItemDisplay");
+// cookingtable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("cooking").style.display = "block";
+// });
+// cookingtable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("cooking").style.display = "none";
+// });
+
+
+// let furnituretable = document.getElementById("FurnitureItemDisplay");
+// furnituretable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("furniture").style.display = "block";
+// });
+// furnituretable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("furniture").style.display = "none";
+// });
+
+
+// let accessoriestable = document.getElementById("AccessoriesItemDisplay");
+// accessoriestable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Accessories").style.display = "block";
+// });
+// accessoriestable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Accessories").style.display = "none";
+// });
+
+// let fashiontable = document.getElementById("FashionItemDisplay");
+// fashiontable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Fashion").style.display = "block";
+// });
+// fashiontable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Fashion").style.display = "none";
+// });
+
+// let clockstable = document.getElementById("ClocksItemDisplay");
+// clockstable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Clock").style.display = "block";
+// });
+// clockstable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Clock").style.display = "none";
+// });
+
+// let lightingtable = document.getElementById("LightingItemDisplay");
+// lightingtable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Lighting").style.display = "block";
+// });
+// lightingtable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Lighting").style.display = "none";
+// });
+
+// let toystable = document.getElementById("ToysItemDisplay");
+// toystable.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Toys").style.display = "block";
+// });
+// toystable.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("Toys").style.display = "none";
+// });
+
+// let shopicon = document.getElementById("ShopIcon");
+// shopicon.addEventListener("mouseover", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("ShopMenu").style.display = "block";
+// });
+// shopicon.addEventListener("mouseleave", function() {
+//     console.log("event taking check")
+//     // highlight the mouseover target
+//     document.getElementById("ShopMenu").style.display = "none";
+// });
